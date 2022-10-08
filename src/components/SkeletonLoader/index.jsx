c76453a88f3f6b47eb25df546db65bf7e12d170c
Mochrizaldianandakurnia <mochrizaldianandakurnia21@gmail.com>
2022-10-08 21:35:01 +0700

    add Skeleton loader

--- src/components/SkeletonLoader/index.jsx
+++ src/components/SkeletonLoader/index.jsx
@@ -1,6 +1,26 @@
+import './index.css'
+
 const index = () => {
   return (
-    <div>index</div>
+    <div className='container skeleton-loader'>
+      <div className='box'>
+        <div className='opener'>
+          <div className='profile-pic'></div>
+          <div className='profile'>
+            <div className='name'></div>
+            <div className='location'></div>
+            <div className='username'></div>
+            <div className='bio'></div>
+          </div>
+        </div>
+        <div className='closer'>
+          <div className='followers'></div>
+          <div className='following'></div>
+          <div className='repo'></div>
+          <div className='link'></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
